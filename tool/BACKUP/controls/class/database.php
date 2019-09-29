58a454921529d8403ad3db626a80f4cd7d526996
Paul Huynh <ginniration@gmail.com>
2019-09-29 09:14:29 +0700

    Remove something no need

--- tool/BACKUP/controls/class/database.php
+++
@@ -1,8 +0,0 @@
-<?
-DEFINE('HOST', 'localhost');
-DEFINE('USRNM', 'root');
-DEFINE('PSWD', '');
-DEFINE('DBNM', 'imt');
-$db = mysqli_connect(HOST,USRNM,PSWD,DBNM) or die("Không thể kết nối database");
-mysqli_set_charset($db,"utf8");
-?>
--- tool/BACKUP/controls/settings.php
+++
@@ -1,7 +0,0 @@
-<?
-	require_once('class/title.php');
-	require_once('class/func.php');
-	require_once('class/auth.php');
-	require_once('class/database.php');
-	require_once('class/functiondb.php');
-?>
